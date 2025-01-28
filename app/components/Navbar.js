f7244ee5d17d7dd079384567114a61b876feea4c
UMANG-2003 <umangkumar20003@gmail.com>
2025-01-29 00:10:15 +0530

    added

---
+++ app/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='bg-gray-800 w-full h-16 flex items-center justify-between'>
+        
+    </nav>
+  )
+}
+
+export default Navbar
